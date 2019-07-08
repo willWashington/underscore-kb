e3906e49bb630cc4f1972bbda20530b52ab77c07
William Washington <william.washington@live.com>
2019-07-08 14:00:30 -0400

    Began working on new cardSlider.js script. Pulled away, cannot continue currently.

--- scripts/cardSlider.js
+++ scripts/cardSlider.js
@@ -1,25 +1,31 @@
-$('.box').click(function() {
-    $('.box').each( function() {
-        if ($(this).offset().left < 0) {            
-            $(this).css("left", "150%");
-        }
-    });
+var allBoxes = $(".box");
 
-    //off-screen speed controller
-    $(this).animate({
-         left: '-50%'
-     }, 500);
+
+
+
+
+// $('.box').click(function() {
+//     $('.box').each( function() {
+//         if ($(this).offset().left < 0) {            
+//             $(this).css("left", "150%");
+//         }
+//     });
+
+//     //off-screen speed controller
+//     $(this).animate({
+//          left: '-50%'
+//      }, 500);
  
 
-     if ($(this).next().length > 0) {
-         $(this).next().animate({
-             left: '30%'
-         }, 500);
-     } else {
-         $(this).prevAll().last().animate({
-             left: '30%'
-         }, 500);
-     }
-});
+//      if ($(this).next().length > 0) {
+//          $(this).next().animate({
+//              left: '30%'
+//          }, 500);
+//      } else {
+//          $(this).prevAll().last().animate({
+//              left: '30%'
+//          }, 500);
+//      }
+// });
 
 
