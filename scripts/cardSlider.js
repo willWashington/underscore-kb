d15283f0e500ddafab6e9a34ab6439a6553c08ab
will.Washington <william.washington@live.com>
2019-07-09 01:09:02 -0400

    Worked on building new slider. Decided on building fade via jquery. Fade is working until the iteration loops back around to the first card displayed. This card will not show.

--- scripts/cardSlider.js
+++ scripts/cardSlider.js
@@ -1,5 +1,34 @@
 var allBoxes = $(".box");
+let counter = 0;
 
+allBoxes.each(function( index ) {
+    if (this.id != "active-card") {
+        $(this).hide();
+    }
+  });
+
+  $('.box').click(function() {
+    $( "#active-card" ).hide("fast");
+    $( "#active-card" ).remove();
+    counter++;
+    if (counter == allBoxes.length) {
+        counter = 0;
+        console.log(counter);
+    }
+    var newActive = $( allBoxes ).get( counter );
+    newActive.id = "active-card";
+    $( "#active-card" ).show("fast");
+    console.log($(newActive).is(":visible")); 
+  })
+  //this is working but when it should show the first element again, it doesn't show anything at all.
+  //the log here shows that the first element (the reset in the loop) is not visible - the code is not working on the iteration
+
+  
+
+//get all boxes - x
+//if a box is clicked on, slide it off screen and hide it
+//count how many boxes have been clicked on
+//show, slide in, and display the box that is next in line
 
 
 
