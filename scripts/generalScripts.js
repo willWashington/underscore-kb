e0d2cc60448a7e8896c97b464faea060bc443fea
will.Washington <william.washington@live.com>
2019-07-12 19:54:59 -0400

    Adjusted generalScripts.js to fix text in Dark Mode/Light Mode button not swapping.
    Created search.js.
    
    Created search.html.
    
    Cleaned up and organized main.css

--- scripts/generalScripts.js
+++ scripts/generalScripts.js
@@ -1,7 +1,8 @@
+var swap = false;
 $('.light-mode').click(function() { //when the user clicks a box
     $('body').toggleClass("background-color-dark background-color");
     $('.page-container').toggleClass("border-gray border-white");
-    var swap = !swap;
+    swap = !swap;
     if (swap) {
         $(this).text("Dark Mode");
     } else {
