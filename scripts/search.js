856b39c9c8463ac570c7b5fb81b4fbda5225cf13
will.Washington <william.washington@live.com>
2019-07-14 10:09:22 -0400

    Updated pathing in seach.js for new domain change. Testing

--- scripts/search.js
+++ scripts/search.js
@@ -5,31 +5,32 @@ jQuery.expr[':'].containsInsensitive = function(a, i, m) {
         .indexOf(m[3].toUpperCase()) >= 0;
 };
 
+var found = false;
 $(document).keyup(function(event) {
     if ($(".search").is(":focus") && event.key == "Enter") {
         var text = $('.search').val(); //declare variable that contains the search string
-        $( "#testDivIndex" ).load( "https://willwashington.github.io/underscore-kb/index.html article", function() {
+        $( "#testDivIndex" ).load( "https://kb.badtuts.com/index.html article", function() {
+            //^ get the article elements from the index.html site and load them in to the #testDiv element
+            $('.box').hide(); //hide all of the boxes
+            $('.box:containsInsensitive("'+text+'")').show('slow'); //show all the boxes that contain the search string
+        } );
+
+        $( "#testDivHtml" ).load( "https://kb.badtuts.com/sites/html.html article", function() {
             //^ get the article elements from the html.html site and load them in to the #testDiv element
             $('.box').hide(); //hide all of the boxes
-            $('.box:containsInsensitive("'+text+'")').show('slow'); //show all the boxes that contains the search string    
+            $('.box:containsInsensitive("'+text+'")').show('slow'); //show all the boxes that contain the search string
         } );
 
-        $( "#testDivHtml" ).load( "https://willwashington.github.io/underscore-kb/sites/html.html article", function() {
-            //^ get the article elements from the html.html site and load them in to the #testDiv element
+        $( "#testDivCSS" ).load( "https://kb.badtuts.com/sites/css.html article", function() {
+            //^ get the article elements from the css.html site and load them in to the #testDiv element
             $('.box').hide(); //hide all of the boxes
-            $('.box:containsInsensitive("'+text+'")').show('slow'); //show all the boxes that contains the search string    
+            $('.box:containsInsensitive("'+text+'")').show('slow'); //show all the boxes that contain the search string    
         } );
 
-        $( "#testDivCSS" ).load( "https://willwashington.github.io/underscore-kb/sites/css.html article", function() {
-            //^ get the article elements from the html.html site and load them in to the #testDiv element
-            $('.box').hide(); //hide all of the boxes        
-            $('.box:containsInsensitive("'+text+'")').show('slow'); //show all the boxes that contains the search string    
-        } );
-
-        $( "#testDivJS" ).load( "https://willwashington.github.io/underscore-kb/sites/js.html article", function() {
-            //^ get the article elements from the html.html site and load them in to the #testDiv element
-            $('.box').hide(); //hide all of the boxes        
-            $('.box:containsInsensitive("'+text+'")').show('slow'); //show all the boxes that contains the search string    
+        $( "#testDivJS" ).load( "https://kb.badtuts.com/sites/js.html article", function() {
+            //^ get the article elements from the js.html site and load them in to the #testDiv element
+            $('.box').hide(); //hide all of the boxes
+            $('.box:containsInsensitive("'+text+'")').show('slow'); //show all the boxes that contain the search string
         } );
     }
 });
@@ -45,7 +46,7 @@ $(document).keyup(function(event) {
 
 
 
-// $( "#testDiv" ).load( "https://willwashington.github.io/underscore-kb/sites/html.html article", function() {
+// $( "#testDiv" ).load( "https://kb.badtuts.com/sites/html.html article", function() {
 //     //^ get the article elements from the html.html site and load them in to the #testDiv element
 //     $('.box').hide(); //hide all of the boxes
 //     var searching = false;
